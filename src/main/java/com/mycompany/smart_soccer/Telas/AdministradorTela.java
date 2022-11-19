15c6533452e5f7498c5ca6efc22cf9bcee42b9c9
João Pedro <joaopedrodamaiamoura@outlook.com>
2022-11-19 00:27:52 -0300

    Atualização geral

--- src/main/java/com/mycompany/smart_soccer/Telas/AdministradorTela.java
+++ src/main/java/com/mycompany/smart_soccer/Telas/AdministradorTela.java
@@ -202,8 +202,6 @@ public class AdministradorTela extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        CadastrarTime telaCadastroT = new CadastrarTime();
-        telaCadastroT.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
